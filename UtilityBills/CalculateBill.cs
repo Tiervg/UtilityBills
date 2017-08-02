b20478705493b212c97b44783eaa24dae7ec7144
Tier <tiervg@gmail.com>
2017-08-02 18:41:02 +0300

    Commit

---
+++ UtilityBills/CalculateBill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityBills
+{
+    class CalculateBill
+    {
+        int counterEnergyLast, counterEnergyNow, counterWaterLast, counterWaterNow, counterSewageLast, counterSewageNow,
+            differenceEnergy, differenceWater, differenceSewage, differenceGas;
+
+        double 
+    }
+}
